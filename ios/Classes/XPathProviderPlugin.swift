6fd1d18a9c2346ce4044e67e2053a6a6ba409e3b
mohbasirudin <152410101076@mail.unej.ac.id>
2024-02-15 17:28:11 +0700

    ios

--- ios/Classes/XPathProviderPlugin.swift
+++ ios/Classes/XPathProviderPlugin.swift
@@ -2,24 +2,48 @@ import Flutter
 import UIKit
 
 public class XPathProviderPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "x_path_provider", binaryMessenger: registrar.messenger())
-    let instance = XPathProviderPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    case "getAppDirectory":
-      result(getAppDirectory())
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "x_path_provider", binaryMessenger: registrar.messenger())
+        let instance = XPathProviderPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
-
-  public func getAppDirectory()-> String {
-  return "ini path directory =="
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "getAppDirectory":
+            result(getAppDirectory())
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    public func getAppDirectory()-> String? {
+//        var path = getDirectory(ofType: fileManagerDirectoryForType(type))
+        var path = getDirectory(ofType: FileManager.SearchPathDirectory.documentDirectory)
+    return path
+    }
+    
+//    private func fileManagerDirectoryForType(_ type: DirectoryType) -> FileManager.SearchPathDirectory {
+//      switch type {
+//      case .applicationCache:
+//        return FileManager.SearchPathDirectory.cachesDirectory
+//      case .applicationDocuments:
+//        return FileManager.SearchPathDirectory.documentDirectory
+//      case .applicationSupport:
+//        return FileManager.SearchPathDirectory.applicationSupportDirectory
+//      case .downloads:
+//        return FileManager.SearchPathDirectory.downloadsDirectory
+//      case .library:
+//        return FileManager.SearchPathDirectory.libraryDirectory
+//      case .temp:
+//        return FileManager.SearchPathDirectory.cachesDirectory
+//      }
+//    }
+    
+    private func getDirectory(ofType directory: FileManager.SearchPathDirectory)-> String?{
+        let paths = NSSearchPathForDirectoriesInDomains(directory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        return paths.first
+    }
 }
